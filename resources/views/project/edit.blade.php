26ae861f6783bc786882a4ef343c58d32be60da0
zakaria-elkoh <zakaria.developer00@gmail.com>
2024-02-15 21:32:19 +0100

    added some changes

--- resources/views/project/edit.blade.php
+++ resources/views/project/edit.blade.php
@@ -8,7 +8,6 @@
     <h2 class="text-3xl font-extrabold mb-5 text-center dark:text-white">Edit a Project:</h2>
     <div>
         <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title:</label>
-        {{-- <input type="number" name="partner_id" value="1"class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title"> --}}
         <input type="text" name="title" value="{{$project->title}}" id="title" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title">
         @error('title')
             <span class="text-red-400">{{$message}}</span>
